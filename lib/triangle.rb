0309aa7763e061848abaace2fa63b05cd32466f5
Brian Diaz <bidiaz101@gmail.com>
2021-10-18 23:06:52 -0500

    commit tree c0a53aefeab7f59ea5def1f7c37d202845b83627

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_reader :side, :side_1, :side_2
+
+  def initialize(side, side_1, side_2)
+    @side = side
+    @side_1 = side_1
+    @side_2 = side_2
+    
+    if self.side + self.side_1 <= self.side_2 || self.side + self.side_2 <= self.side_1 || self.side_1 + self.side_2 <= self.side || self.side <= 0 || self.side_1 <= 0 || self.side_2 <= 0
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if self.side == self.side_1 && self.side == self.side_2
+      :equilateral
+    elsif self.side == self.side_1 || self.side == self.side_2 || self.side_1 == self.side_2
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
